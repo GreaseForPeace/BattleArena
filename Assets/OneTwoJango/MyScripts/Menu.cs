e110b8bca5ff93ae52e996cb050c4b6857984baf
GodCreator <golearn121@gmail.com>
2015-06-25 02:35:22 +0300

    Menu

--- Assets/OneTwoJango/MyScripts/Menu.cs
+++ Assets/OneTwoJango/MyScripts/Menu.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour 
+{
+    private Animator _animator;
+    private CanvasGroup _canvasGroup;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool IsOpen
+    {
+        get { return _animator.GetBool("IsOpen"); }
+        set { _animator.SetBool("IsOpen", value); }
+    }
+
+    public void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+    }
+
+    public void Update()
+    {
+        if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+        {
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+        }
+        else
+        {
+            _canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+        }
+    }
 }
