78d6efc367fc4090b4d1d4faa5039af6e8460985
GodCreator <golearn121@gmail.com>
2015-06-26 05:07:05 +0300

    Interface

---
+++ Assets/OneTwoJango/MyScripts/ChangeQuality.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ChangeQuality : MonoBehaviour
+{
+
+    public void Quality(int level)
+    {
+        
+            QualitySettings.SetQualityLevel(level);
+     }
+
+        
+}
